74d12bdbd499e3aa447aa31254d0258ee65c9f4a
Mike Spivey <mike@cs.ox.ac.uk>
2018-01-25 13:09:34 +0000

    Script changes for armtest

---
+++ armtest/config.h
@@ -0,0 +1,62 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* Define to set up for debugging */
+/* #undef DEBUG */
+
+/* Define to 1 if you don't have `vprintf' but do have `_doprnt.' */
+/* #undef HAVE_DOPRNT */
+
+/* Define to 1 if you have the `getpagesize' function. */
+#define HAVE_GETPAGESIZE 1
+
+/* Define to 1 if you have the `mmap' function. */
+#define HAVE_MMAP 1
+
+/* Define to 1 if you have the `sigprocmask' function. */
+#define HAVE_SIGPROCMASK 1
+
+/* Define to 1 if you have the `vprintf' function. */
+#define HAVE_VPRINTF 1
+
+/* Define for Linux */
+#define LINUX 1
+
+/* Log2 of page size */
+#define LOG_PAGESIZE 12
+
+/* Define to enable hacks for MacOS X */
+/* #undef MACOS */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "mike@cs.ox.ac.uk"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "thunder"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "thunder 0.2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "thunder"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.2"
+
+/* Page size */
+#define PAGESIZE 4096
+
+/* Whether to flush the cache */
+/* #undef USE_FLUSH */
+
+/* Whether to call mprotect from JIT */
+#define USE_MPROTECT 1
+
+/* Define to enable hacks for Windows */
+/* #undef WINDOWS */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
